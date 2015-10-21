a72c460367efb887af6ef29c59856127424ec32b
naeem <naeemahemad.mansuri@infostretch.com>
2015-10-21 12:20:10 +0530

    gitignore update and added 101 Test case.

---
+++ src/com/testingproject/pages/HomePage.java
@@ -0,0 +1,15 @@
+package com.testingproject.pages;
+
+import com.infostretch.automation.ui.WebDriverBaseTestPage;
+import com.infostretch.automation.ui.api.PageLocator;
+import com.infostretch.automation.ui.api.WebDriverTestPage;
+
+public class HomePage extends WebDriverBaseTestPage<WebDriverTestPage> {
+
+	@Override
+	protected void openPage(PageLocator locator, Object... args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/com/testingproject/step/WebSiteTestStep.java
@@ -0,0 +1,17 @@
+package com.testingproject.step;
+
+import com.infostretch.automation.step.QAFTestStep;
+
+public class WebSiteTestStep {
+
+	@QAFTestStep(description = "customer open google home page")
+	public void openPage() {
+		System.out.println("customer launch home page of website");
+	}
+
+	@QAFTestStep(description = "verify customer is on home page")
+	public void verifyHomePage() {
+		System.out.println("verification logic goes here");
+	}
+
+}
